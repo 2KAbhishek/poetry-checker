341f6ba9f905741d848faa2565ea8cba862058ea
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-04-05 17:42:15 +0530

    Add poetry_functions.py

---
+++ src/poetry_functions.py
@@ -0,0 +1,253 @@
+"""CSC108H1: Assignment 3: Poetry Form Checker
+
+Instructions (READ THIS FIRST!)
+===============================
+
+Make sure that the code files:
+    poetry_constants.py, poetry_program.py, poetry_reader.py,
+    test_get_last_syllable.py,
+    a3_checker.py, a3_pyta.json and checker_generic.py,
+the folder dataset that includes files:
+    pronunication_dictionary.txt, pronunication_dictionary_small.txt,
+    poetry_forms.txt and poetry_forms_small.txt, and
+the folder sample_poems that includes files:
+    haiku1.txt, haiku2.txt, etc.
+are in the same folder as this file (poetry_functions.py).
+
+Copyright and Usage Information
+===============================
+
+This code is provided solely for the personal and private use of students
+taking the course CSC108 at the University of Toronto. Copying for purposes
+other than this use is expressly prohibited. All forms of distribution of
+this code, whether as given or with any changes, are expressly prohibited.
+
+All of the files in this folder are:
+Copyright (c) 2022 the University of Toronto CSC108 Teaching Team.
+"""
+
+from typing import List, Tuple, Dict
+
+from poetry_constants import (POEM_LINE, POEM, PHONEMES, PRONUNCIATION_DICT,
+                              POETRY_FORM_DESCRIPTION)
+
+# ===================== Provided Helper Functions =====================
+
+
+def transform_string(s: str) -> str:
+    """Return a new string based on s in which all letters have been
+    converted to uppercase and punctuation characters have been stripped
+    from both ends. Inner punctuation is left untouched.
+
+    >>> transform_string('Birthday!!!')
+    'BIRTHDAY'
+    >>> transform_string('"Quoted?"')
+    'QUOTED'
+    >>> transform_string('To be? Or not to be?')
+    'TO BE? OR NOT TO BE'
+    """
+
+    punctuation = """!"'`@$%^&_-+={}|\\/,;:.-?)([]<>*#\n\t\r"""
+    result = s.upper().strip(punctuation)
+    return result
+
+
+def is_vowel_phoneme(phoneme: str) -> bool:
+    """Return True if and only if phoneme is a vowel phoneme. That is, whether
+    phoneme ends in a 0, 1, or 2.
+
+    Precondition: len(phoneme) > 0 and phoneme.isupper()
+
+    >>> is_vowel_phoneme('AE0')
+    True
+    >>> is_vowel_phoneme('DH')
+    False
+    >>> is_vowel_phoneme('IH2')
+    True
+    """
+
+    return phoneme[-1] in '012'
+
+
+# ===================== Add Your Helper Functions Here =====================
+
+
+# ===================== Required Functions =================================
+
+
+# Functions related to syllable counts
+
+
+def get_syllable_count(poem_line: POEM_LINE,
+                       words_to_phonemes: PRONUNCIATION_DICT) -> int:
+    """Return the number of syllables in poem_line by using the pronunciation
+    dictionary words_to_phonemes.
+
+    Precondition: len(poem_line) > 0
+
+    >>> line = 'Then! the #poem ends.'
+    >>> word_to_phonemes = {'THEN': ('DH', 'EH1', 'N'),
+    ...                     'ENDS': ('EH1', 'N', 'D', 'Z'),
+    ...                     'THE': ('DH', 'AH0'),
+    ...                     'POEM': ('P', 'OW1', 'AH0', 'M')}
+    >>> get_syllable_count(line, word_to_phonemes)
+    5
+    """
+
+
+def check_syllable_counts(poem_lines: POEM,
+                          description: POETRY_FORM_DESCRIPTION,
+                          word_to_phonemes: PRONUNCIATION_DICT) \
+        -> List[POEM_LINE]:
+    """Return a list of lines from poem_lines that do NOT have the right
+    number of syllables as specified by the poetry form description, according
+    to the pronunciation dictionary word_to_phonemes.  If all lines have the
+    right number of syllables, return the empty list.
+
+    Precondition: len(poem_lines) == len(description[0])
+
+    >>> poem_lines = ['The first line leads off,',
+    ...               'With a gap before the next.', 'Then the poem ends.']
+    >>> description = ((5, 5, 4), ('*', '*', '*'))
+    >>> word_to_phonemes = {'NEXT': ('N', 'EH1', 'K', 'S', 'T'),
+    ...                     'GAP': ('G', 'AE1', 'P'),
+    ...                     'BEFORE': ('B', 'IH0', 'F', 'AO1', 'R'),
+    ...                     'LEADS': ('L', 'IY1', 'D', 'Z'),
+    ...                     'WITH': ('W', 'IH1', 'DH'),
+    ...                     'LINE': ('L', 'AY1', 'N'),
+    ...                     'THEN': ('DH', 'EH1', 'N'),
+    ...                     'THE': ('DH', 'AH0'),
+    ...                     'A': ('AH0'),
+    ...                     'FIRST': ('F', 'ER1', 'S', 'T'),
+    ...                     'ENDS': ('EH1', 'N', 'D', 'Z'),
+    ...                     'POEM': ('P', 'OW1', 'AH0', 'M'),
+    ...                     'OFF': ('AO1', 'F')}
+    >>> check_syllable_counts(poem_lines, description, word_to_phonemes)
+    ['With a gap before the next.', 'Then the poem ends.']
+    >>> poem_lines = ['The first line leads off,']
+    >>> description = ((0,), ('*'))
+    >>> check_syllable_counts(poem_lines, description, word_to_phonemes)
+    []
+    """
+
+
+# Functions related to rhyming
+
+
+def get_last_syllable(word_phonemes: PHONEMES) -> PHONEMES:
+    """Return the last syllable from word_phonemes.
+
+    The last syllable in word_phonemes is formed from the last vowel phoneme
+    and any subsequent consonant phoneme(s) in word_phonemes, in the same
+    order as they appear in word_phonemes.
+
+    >>> get_last_syllable(('AE1', 'B', 'S', 'IH0', 'N', 'TH'))
+    ('IH0', 'N', 'TH')
+    >>> get_last_syllable(('IH0', 'N'))
+    ('IH0', 'N')
+    >>> get_last_syllable(('B', 'S'))
+    ()
+    """
+
+
+def words_rhyme(word1: str, word2: str, word_to_phonemes: PRONUNCIATION_DICT) \
+        -> bool:
+    """Return True if and only if word1 and word2 rhyme according to
+    word_to_phonemes.
+
+    Recall that two words rhyme if and only if they have the same last
+    syllable.
+
+    >>> word_to_phonemes = {'THINE': ('DH', 'AY1', 'N'),
+    ...                     'DEVINE': ('D', 'AH0', 'V', 'AY1', 'N'),
+    ...                     'HEARD': ('HH', 'ER1', 'D')}
+    >>> words_rhyme('thine', 'devine', word_to_phonemes)
+    True
+    >>> words_rhyme('thine', 'heard', word_to_phonemes)
+    False
+    """
+
+
+def all_lines_rhyme(poem_lines: POEM, lines_to_check: List[int],
+                    word_to_phonemes: PRONUNCIATION_DICT) -> bool:
+    """Return True if and only if the lines from poem_lines with index in
+    lines_to_check all rhyme, according to word_to_phonemes.
+
+    Precondition: lines_to_check != []
+
+    >>> poem_lines = ['The mouse', 'in my house', 'electric.']
+    >>> lines_to_check = [0, 1]
+    >>> word_to_phonemes = {'THE': ('DH', 'AH0'),
+    ...                     'MOUSE': ('M', 'AW1', 'S'),
+    ...                     'IN': ('IH0', 'N'),
+    ...                     'MY': ('M', 'AY1'),
+    ...                     'HOUSE': ('HH', 'AW1', 'S'),
+    ...                     'ELECTRIC': ('IH0', 'L', 'EH1', 'K',
+    ...                                  'T', 'R', 'IH0', 'K')}
+    >>> all_lines_rhyme(poem_lines, lines_to_check, word_to_phonemes)
+    True
+    >>> lines_to_check = [0, 1, 2]
+    >>> all_lines_rhyme(poem_lines, lines_to_check, word_to_phonemes)
+    False
+    >>> lines_to_check = [2]
+    >>> all_lines_rhyme(poem_lines, lines_to_check, word_to_phonemes)
+    True
+    """
+
+
+def get_symbol_to_lines(rhyme_scheme: Tuple[str]) -> Dict[str, List[int]]:
+    """Return a dictionary where each key is an item in rhyme_scheme and
+    its corresponding value is a list of the indexes in rhyme_scheme where
+    the item appears.
+
+    >>> result = get_symbol_to_lines(('A', 'A', 'B', 'B', 'A'))
+    >>> expected = {'A': [0, 1, 4], 'B': [2, 3]}
+    >>> expected == result
+    True
+    >>> result = get_symbol_to_lines(('*', '*', '*', '*', '*'))
+    >>> expected = {'*': [0, 1, 2, 3, 4]}
+    >>> expected == result
+    True
+    """
+
+
+def check_rhyme_scheme(poem_lines: POEM,
+                       description: POETRY_FORM_DESCRIPTION,
+                       word_to_phonemes: PRONUNCIATION_DICT) \
+        -> List[List[POEM_LINE]]:
+    """Return a list of lists of lines from poem_lines that do NOT rhyme with
+    each other as specified by the poetry form description, according to the
+    pronunciation dictionary word_to_phonemes. If all lines rhyme as they
+    should, return the empty list.  The lines is each inner list should be
+    in the same order as they appear in poem_lines.
+
+    Precondition: len(poem_lines) == len(description[1])
+
+    >>> poem_lines = ['The first line leads off,',
+    ...               'With a gap before the next.', 'Then the poem ends.']
+    >>> description = ((5, 7, 5), ('A', 'B', 'A'))
+    >>> word_to_phonemes = {'NEXT': ('N', 'EH1', 'K', 'S', 'T'),
+    ...                     'GAP': ('G', 'AE1', 'P'),
+    ...                     'BEFORE': ('B', 'IH0', 'F', 'AO1', 'R'),
+    ...                     'LEADS': ('L', 'IY1', 'D', 'Z'),
+    ...                     'WITH': ('W', 'IH1', 'DH'),
+    ...                     'LINE': ('L', 'AY1', 'N'),
+    ...                     'THEN': ('DH', 'EH1', 'N'),
+    ...                     'THE': ('DH', 'AH0'),
+    ...                     'A': ('AH0'),
+    ...                     'FIRST': ('F', 'ER1', 'S', 'T'),
+    ...                     'ENDS': ('EH1', 'N', 'D', 'Z'),
+    ...                     'POEM': ('P', 'OW1', 'AH0', 'M'),
+    ...                     'OFF': ('AO1', 'F')}
+    >>> bad_lines = check_rhyme_scheme(poem_lines, description,
+    ...                                word_to_phonemes)
+    >>> bad_lines.sort()
+    >>> bad_lines
+    [['The first line leads off,', 'Then the poem ends.']]
+    """
+
+
+if __name__ == '__main__':
+    import doctest
+    # Uncomment the line below if you prefer to test your examples with doctest
+    # doctest.testmod()
