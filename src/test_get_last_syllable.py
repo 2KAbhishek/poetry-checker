8855fef9e179be1373c87b86a8cd4870417d4f25
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-04-06 16:58:38 +0530

    Add test for reading poetry form details

--- src/test_get_last_syllable.py
+++ src/test_get_last_syllable.py
@@ -69,13 +69,27 @@ class TestReadPronunciation(unittest.TestCase):
     def test_read_pronunciation(self):
         """Test phonemes on a pronunciation dictionary."""
         import os
-        small_pd = open(os.path.dirname(__file__) + '/datasets/pronunciation_dictionary_small.txt')
+        small_pd = open(os.path.dirname(__file__) +
+                        '/datasets/pronunciation_dictionary_small.txt')
         actual = poetry_reader.read_pronunciation(small_pd)
 
         expected = {'CAMPBELL': ('K', 'AE1', 'M', 'B', 'AH0', 'L'),
                     'GRIES': ('G', 'R', 'AY1', 'Z'),
                     'SMITH': ('S', 'M', 'IH1', 'TH')}
         self.assertEqual(actual, expected, 'read pronunciation')
+
+
+class TestReadPoetryFormDetails(unittest.TestCase):
+    def test_read_poetry_form_details(self):
+        """Test whether the poetry form details are read correctly."""
+        import os
+        small_pf = open(os.path.dirname(__file__) +
+                        '/datasets/poetry_forms_small.txt')
+        actual = poetry_reader.read_poetry_form_descriptions(small_pf)
+        expected = {
+            'Haiku': ((5, 7, 5), ('*', '*', '*')),
+            'Limerick': ((8, 8, 5, 5, 8), ('A', 'A', 'B', 'B', 'A'))}
+        self.assertEqual(actual, expected, 'read poetry form details')
 
 
 # Place your unit test definitions before this line.
