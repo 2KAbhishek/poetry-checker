c082239ab84037aea88dae7978979db204c8d99c
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-04-08 15:43:29 +0530

    Move each module test to seperate file

--- test/test_poetry_functions.py
+++ test/test_poetry_functions.py
@@ -1,8 +1,8 @@
+import src.poetry_functions as poetry_functions
 import os.path
 import unittest
-
-import src.poetry_functions as poetry_functions
-import src.poetry_reader as poetry_reader
+import sys
+sys.path.append(".")
 
 
 class TestGetLastSyllable(unittest.TestCase):
@@ -161,34 +161,6 @@ class TestCheckRhymeScheme(unittest.TestCase):
         actual.sort()
         expected = [['The first line leads off,', 'Then the poem ends.']]
         self.assertEqual(actual, expected, 'check rhyme scheme')
-
-
-class TestReadPronunciation(unittest.TestCase):
-    def test_read_pronunciation(self):
-        """Test phonemes on a pronunciation dictionary."""
-        import os
-        small_pd = open(os.path.dirname(__file__) +
-                        '/../data/pronunciation_dictionary_small.txt')
-        actual = poetry_reader.read_pronunciation(small_pd)
-
-        expected = {'CAMPBELL': ('K', 'AE1', 'M', 'B', 'AH0', 'L'),
-                    'GRIES': ('G', 'R', 'AY1', 'Z'),
-                    'SMITH': ('S', 'M', 'IH1', 'TH')}
-        self.assertEqual(actual, expected, 'read pronunciation')
-
-
-class TestReadPoetryFormDetails(unittest.TestCase):
-    def test_read_poetry_form_details(self):
-        """Test whether the poetry form details are read correctly."""
-        import os
-        small_pf = open(os.path.dirname(__file__) +
-                        '/../data/poetry_forms_small.txt')
-        actual = poetry_reader.read_poetry_form_descriptions(small_pf)
-        expected = {
-            'Haiku': ((5, 7, 5), ('*', '*', '*')),
-            'Limerick': ((8, 8, 5, 5, 8), ('A', 'A', 'B', 'B', 'A'))}
-        self.assertEqual(actual, expected, 'read poetry form details')
-
 
 # Place your unit test definitions before this line.
 if __name__ == '__main__':
